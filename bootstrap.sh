902f706f5685c0642480a53c9d1b77c52f30d32a
gdisastery <gye@student.tgm.ac.at>
2014-05-27 23:39:49 +0200

    Renaming the directory in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ wget https://github.com/yiisoft/yii/releases/download/1.1.14/yii-1.1.14.f0fee9.t
 tar xf yii-1.1.14.f0fee9.tar.gz
 
 # Create a link /home/vagrant/opt/yii pointing to the yii directory
-ln -s yii-1.1.14.f0fee9 yii
-chown www-data yii
-chgrp www-data yii
+mv yii-1.1.14.f0fee9 yii
+chown -R www-data yii
+chgrp -R www-data yii
 
