2ed760ec6a91b0957e02d0d098182bfea4b04217
gdisastery <gye@student.tgm.ac.at>
2014-05-29 11:32:49 +0200

    Enabled debugging mode

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 // change the following paths if necessary
 $yii='/home/vagrant/opt/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
