b6812dfb5954310623f4fb212123ceddf752ac79
gdisastery <gye@student.tgm.ac.at>
2014-05-31 01:24:13 +0200

    Added list for event form using JS.

--- src/protected/views/event/_form.php
+++ src/protected/views/event/_form.php
@@ -1,7 +1,25 @@
+
+
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'event-form',
 	'enableAjaxValidation'=>false,
-)); ?>
+));
+
+Yii::app()->clientScript->registerScript('search', "
+$('#btn-add-time').click(function(){
+	$('#onetimeprop:first').clone().appendTo('#timeprops');	
+	return false;
+});
+
+$('#btn-add-user').click(function(){
+	$('#oneuser:first').clone().appendTo('#userinvs');	
+	return false;
+});
+
+
+");
+// http://stackoverflow.com/questions/8298138/cloning-elements-avoiding-more-than-one-clone-at-a-time-when-adding
+?>
 
 	<p class="help-block">Fields with <span class="required">*</span> are required.</p>
 
@@ -11,11 +29,52 @@
 
 	<?php echo $form->textFieldRow($model,'location',array('class'=>'span5','maxlength'=>255)); ?>
 
-	<?php echo $form->textFieldRow($model,'description',array('class'=>'span5','maxlength'=>8192)); ?>
+
+	<?php echo $form->textAreaRow($model,'description',array('class'=>'span8', 'rows'=>5, 'maxlength'=>8192)); ?>
 
 <br/>
+	<div id="timeproposal">
+		Time proposals 
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+					'label'=>'+',
+					'type'=>'info', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
+					'size'=>'mini', // null, 'large', 'small' or 'mini'
+					'htmlOptions'=>array('id'=>'btn-add-time')
+					)); ?>
+		<br/>
 
-	<?php 
+		<!-- todo: we have to load the old data first! -->
+
+		<div id="timeprops">
+			<div id="onetimeprop">
+				<input style="height:20px;" id="publishDate" type="datetime-local" name="appointments[]" class="datetime"> <br/>
+			</div>
+		</div>
+	</div>
+
+	<!-- Invitations -->
+	<div id="userinvitations">
+		Invite users 
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+					'label'=>'+',
+					'type'=>'info', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
+					'size'=>'mini', // null, 'large', 'small' or 'mini'
+					'htmlOptions'=>array('id'=>'btn-add-user')
+					)); ?>
+		<br/>
+
+		<!-- todo: we have to load the old data first! -->
+
+		<div id="userinvs">
+			<div id="oneuser">
+				<input style="height:20px;" type="textfield" name="users[]">  <br/>
+			</div>
+		</div>
+	</div>
+
+
+<?php 
+/*	
 	$this->widget('zii.widgets.jui.CJuiDatePicker',array(
 				'name'=>'publishDate',
 				// additional javascript options for the date picker plugin
@@ -26,6 +85,8 @@
 					'style'=>'height:20px;'
 					),
 				));
+
+*/
 ?>
  
  <!--  No need for host id, it is the current users id.	
