5a7b937e44e13cdd4f628f97ca87a80b6fa9bda6
gdisastery <gye@student.tgm.ac.at>
2014-06-04 21:12:11 +0200

    The name of the event is written on the header.

--- src/protected/views/event/update.php
+++ src/protected/views/event/update.php
@@ -8,6 +8,6 @@ $this->breadcrumbs=array(
 require_once('_options.php');
 ?>
 
-<h1>Update Event <?php echo $model->id; ?></h1>
+<h1>Update <?php echo $model->name; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
