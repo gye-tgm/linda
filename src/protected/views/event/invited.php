c3dc0169372e377cdb10acc4ccef8dcb03b9a78d
gdisastery <gye@student.tgm.ac.at>
2014-06-04 15:38:12 +0200

    Fixed a bug, now the host will be up.

--- src/protected/views/event/invited.php
+++ src/protected/views/event/invited.php
@@ -12,7 +12,6 @@ require_once('_options.php');
 
 <h1>Invited Events</h1>
 
-
 <?php $this->widget('bootstrap.widgets.TbGridView', array(
     'type'=>'striped bordered condensed',
     'dataProvider'=>$dataProvider,
@@ -22,12 +21,12 @@ require_once('_options.php');
 		array(
             'class'=>'CLinkColumn',
             'labelExpression'=>'$data->name',
-			'urlExpression'=>'Yii::app()->createUrl("event/view", array("id"=>$data->id))',
+						'urlExpression'=>'Yii::app()->createUrl("event/view", array("id"=>$data->id))',
             'header'=>'Name',
         ),
 		array('name'=>'location','header'=>'Location'),
 		array('name'=>'description','header'=>'Description'),
-		array('value'=>'User::model()->findByPk($data->id)->username','header'=>'Organisator'),
+		array('value'=>'User::model()->findByPk($data->hostid)->username','header'=>'Organisator'),
 		array(
             'class'=>'CLinkColumn',
             'label'=>'accept',
